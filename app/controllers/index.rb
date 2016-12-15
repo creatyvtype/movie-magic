877244f6f563bd48ef0755d07c9f729a675ced74
Majd Murad <murad.majd@gmail.com>
2016-12-15 00:46:45 -0800

    Basic styled index complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,11 @@
 get '/' do
 	@movies = Tmdb::Movie.popular
 	ap @movies
+	ap Tmdb::Movie.images(@movies.first.id)
 	erb :index
+end
+
+get '/movies/:id' do
+	@movie = Tmdb::Movie.detail(params[:id])
+	erb :show
 end
